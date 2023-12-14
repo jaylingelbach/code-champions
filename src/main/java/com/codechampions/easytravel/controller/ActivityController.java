c6b7b12fcecd69f208256f1a2583e8b2d7c43f07
tripl3crown <intheazvalley@gmail.com>
2023-12-14 00:00:46 -0500

    Start initial project structure

---
+++ src/main/java/com/codechampions/easytravel/controller/ActivityController.java
@@ -0,0 +1,26 @@
+package com.codechampions.easytravel.controller;
+
+import com.codechampions.easytravel.repository.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.codechampions.easytravel.model.Activity;
+import java.util.List;
+
+@RestController
+public class ActivityController {
+
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    @PostMapping("/activity")
+    Activity newActivity(@RequestBody Activity newActivity){
+        return activityRepository.save(newActivity);
+    }
+    @GetMapping("/activities")
+    List<Activity> getAllActivities(){
+        return activityRepository.findAll();
+    }
+}
