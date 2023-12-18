fc9b122072d768be1172a9160c108e85ca13ad11
QuantaQualia <markatkins3000@outlook.com>
2023-12-17 22:38:34 -0600

    Starting on the payment button

---
+++ easytravel/src/checkout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {PayPalButton} from 'react-paypal-button';
+
+const payPalButtonComponent = () => {
+
+    const success;
+    const cancel;
+    const error;
+};
+
+export default payPalButtonComponent;
