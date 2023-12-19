d9e7326db81e1cc5d15bd49f6a06e53475bd8a58
QuantaQualia <markatkins3000@outlook.com>
2023-12-19 15:55:10 -0600

    commit

--- easytravel/src/checkout.js
+++ easytravel/src/checkout.js
@@ -4,14 +4,22 @@ import {PayPalButton} from 'react-paypal-button';
 const payPalButtonComponent = () => {
 
     const success = (payment) => {
+        console.log("Your payment was successful.");
 
     };
     const cancel = (data) => {
+        console.log("Your payment was canceled.");
 
     };
     const error = (err) => {
+        console.err("There was an error with your payment.");
 
     };
+    return (
+        <PayPalButton
+        
+        />
+    );
 };
 
 export default payPalButtonComponent;
