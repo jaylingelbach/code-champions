f94f5e9dfd20c855901d8a44490cec0d474f95c3
Jesse Lawson <jesse.t.lawson@gmail.com>
2024-01-04 19:44:38 -0600

    added comments class and repository

---
+++ back-end/src/main/java/com/codechampions/easytravel/model/Comments.java
@@ -0,0 +1,52 @@
+package com.codechampions.easytravel.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+
+public class Comments {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @OneToMany
+    private Integer userId;
+
+    private String text;
+
+    public Comments() {
+    }
+
+    public Comments(Integer id, Integer userId, String text) {
+        this.id = id;
+        this.userId = userId;
+        this.text = text;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
