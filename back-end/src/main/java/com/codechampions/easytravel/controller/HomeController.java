ff338f16730aeda6a0a7d02cd9d68ad633e003aa
Emily <emily.zy.lin@gmail.com>
2024-01-05 22:04:50 -0600

    Added homepage displaying "123" for test

--- back-end/src/main/java/com/codechampions/easytravel/controller/HomeController.java
+++ back-end/src/main/java/com/codechampions/easytravel/controller/HomeController.java
@@ -1,11 +1,22 @@
 package com.codechampions.easytravel.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
+
     @GetMapping("/")
     public String redirectToHomePage() {
         return "redirect:/welcome";
+    }
+
+
+    @GetMapping("/welcome")
+    public String displayHomePage() {
+        //model.addAttribute("headingText", "Welcome");
+        return "index";
     }
 
 }
