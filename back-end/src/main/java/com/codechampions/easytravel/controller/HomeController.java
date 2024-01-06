19f899166dfef5318d4c913c98fb6218ae661152
Emily <emily.zy.lin@gmail.com>
2024-01-06 16:40:37 -0600

    welcome page ("/welcome")

--- back-end/src/main/java/com/codechampions/easytravel/controller/HomeController.java
+++ back-end/src/main/java/com/codechampions/easytravel/controller/HomeController.java
@@ -1,22 +1,14 @@
 package com.codechampions.easytravel.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-//    @GetMapping("/")
-//    public String redirectToHomePage() {
-//        return "redirect:/welcome";
-//    }
-
-
     @GetMapping("/welcome")
     public String displayHomePage() {
-        //model.addAttribute("headingText", "Welcome");
-        return "index";
+          return "index";
     }
 
 }
