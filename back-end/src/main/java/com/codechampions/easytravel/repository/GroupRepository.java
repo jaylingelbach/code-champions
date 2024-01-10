10e8a39d06519c91587b6c57a18dba57b272b195
tripl3crown <intheazvalley@gmail.com>
2024-01-10 01:57:33 -0500

    created a group table

---
+++ back-end/src/main/java/com/codechampions/easytravel/repository/GroupRepository.java
@@ -0,0 +1,8 @@
+package com.codechampions.easytravel.repository;
+
+import com.codechampions.easytravel.model.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group,Long> {
+
+}
