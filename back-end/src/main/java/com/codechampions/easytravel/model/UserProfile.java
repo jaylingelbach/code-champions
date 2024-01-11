c0f1197655b2b95bd0acf7ab6a77ea5a856c4199
Emily <emily.zy.lin@gmail.com>
2024-01-11 00:16:19 -0600

    added UserProfile class

---
+++ back-end/src/main/java/com/codechampions/easytravel/model/UserProfile.java
@@ -0,0 +1,78 @@
+package com.codechampions.easytravel.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class UserProfile extends AbstractEntity{
+
+    private int userId;
+
+    //    @NotNull
+    private String firstName;
+
+    //    @NotNull
+    private String lastName;
+
+    private String address;
+
+    private String location;
+
+    private String email;
+
+    public UserProfile(int userId, String firstName, String lastName, String address, String location, String email) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.location = location;
+        this.email = email;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
