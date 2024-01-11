751d5cce6cb518ae7e64c7126a57a1d8c48b59a9
Emily <emily.zy.lin@gmail.com>
2024-01-11 00:12:01 -0600

    added UserProfileDTO.java

---
+++ back-end/src/main/java/com/codechampions/easytravel/model/dto/UserProfileDTO.java
@@ -0,0 +1,55 @@
+package com.codechampions.easytravel.model.dto;
+
+public class UserProfileDTO {
+
+    private String firstName;
+
+    //    @NotNull
+    private String lastName;
+
+    private String address;
+
+    private String location;
+
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
