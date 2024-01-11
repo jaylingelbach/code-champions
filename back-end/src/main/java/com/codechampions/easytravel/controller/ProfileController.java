a1050bfb05f6a403366d5273a9950413222eb368
Emily <emily.zy.lin@gmail.com>
2024-01-11 00:27:20 -0600

    corrected controller and add ProfileRepository.java

---
+++ back-end/src/main/java/com/codechampions/easytravel/controller/ProfileController.java
@@ -0,0 +1,49 @@
+package com.codechampions.easytravel.controller;
+
+import com.codechampions.easytravel.model.User;
+import com.codechampions.easytravel.model.UserProfile;
+import com.codechampions.easytravel.model.dto.UserProfileDTO;
+import com.codechampions.easytravel.repository.ProfileRepository;
+import com.codechampions.easytravel.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/profile")
+public class ProfileController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    private static final String userSessionKey = "user";
+
+    @GetMapping("")
+    public String viewProfile (Model model, HttpSession session) {
+
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+
+        Optional<User> userOpt = userRepository.findById(userId);
+        UserProfile userProfile = profileRepository.findByUserId(userId);
+
+        UserProfileDTO userProfileDTO = new UserProfileDTO();
+
+        if (userProfile != null) {
+            userProfileDTO.setFirstName(userProfile.getFirstName());
+        }
+
+        model.addAttribute("username", userOpt.get().getUsername());
+        model.addAttribute("userProfileDTO", userProfileDTO);
+
+        return "profile/view";
+    }
+
+}
