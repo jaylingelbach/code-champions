968ef01375a7f046a774b4d31e33c82c534f9b18
Emily <emily.zy.lin@gmail.com>
2024-01-11 17:57:24 -0600

    added postMapping on ProfileController

--- back-end/src/main/java/com/codechampions/easytravel/controller/ProfileController.java
+++ back-end/src/main/java/com/codechampions/easytravel/controller/ProfileController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.servlet.http.HttpSession;
 
@@ -27,7 +29,7 @@ public class ProfileController {
     private static final String userSessionKey = "user";
 
     @GetMapping("")
-    public String displayProfileForm (Model model, HttpSession session) {
+    public String displayProfileForm(Model model, HttpSession session) {
 
         Integer userId = (Integer) session.getAttribute(userSessionKey);
 
@@ -48,6 +50,30 @@ public class ProfileController {
         model.addAttribute("userProfileDTO", userProfileDTO);
 
         return "profile/form";
+    }
+
+    @PostMapping("")
+    // TODO: add @Valid here
+    public String processProfileForm(@ModelAttribute UserProfileDTO userProfileDTO, HttpSession session) {
+
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+
+        UserProfile userProfile = profileRepository.findByUserId(userId);
+
+        if (userProfile == null) {
+            userProfile = new UserProfile();
+            userProfile.setUserId(userId);
+        }
+
+        userProfile.setFirstName(userProfileDTO.getFirstName());
+        userProfile.setLastName(userProfileDTO.getLastName());
+        userProfile.setAddress(userProfileDTO.getAddress());
+        userProfile.setLocation(userProfileDTO.getLocation());
+        userProfile.setEmail(userProfileDTO.getEmail());
+
+        profileRepository.save(userProfile);
+
+        return "redirect:/profile";
     }
 
 }
