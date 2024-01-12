229aed56a349e99276598d05903c93a9a3334ea5
Jesse Lawson <jesse.t.lawson@gmail.com>
2024-01-11 19:24:38 -0600

    added search button

--- front-end/easytravel/src/components/Event.js
+++ front-end/easytravel/src/components/Event.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+
+
+
 const Event = () => {
   return (
     <div>
       <h2>Event Page</h2>
       <p>Welcome to easyTravel!</p>
       <form>
-        <label><input placeholder='Search'></input>Search</label>
+        <input placeholder='Search'></input>
+        <button>Search</button>
 
       </form>
     </div>
