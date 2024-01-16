9bb7d027138137e5eb4fb95f7f10dba368c8faf7
Jesse Lawson <jesse.t.lawson@gmail.com>
2024-01-15 19:00:58 -0600

    added some logic to the input on event form

--- front-end/easytravel/src/components/Event.js
+++ front-end/easytravel/src/components/Event.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 
 const Event = () => {
+
+
+  const [search, setSearch] = useState('');
+
   return (
     <div>
       <h2>Event Page</h2>
       <p>Welcome to easyTravel!</p>
       <form>
-        <input placeholder='Search'></input>
+        <input id='search'
+        name='search'
+        type='text'  
+        value={search}
+        placeholder='Search'
+        onChange={event => setSearch(event.target.value)}></input>
         <button>Search</button>
 
       </form>
