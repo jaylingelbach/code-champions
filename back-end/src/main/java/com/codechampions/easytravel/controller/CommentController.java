05e55d465a536bd5a771bac89afef9333d4922fa
Emily <emily.zy.lin@gmail.com>
2024-01-17 17:14:08 -0600

    added CommentRespository to CommentController.java

--- back-end/src/main/java/com/codechampions/easytravel/controller/CommentController.java
+++ back-end/src/main/java/com/codechampions/easytravel/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.codechampions.easytravel.controller;
 
+import com.codechampions.easytravel.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/comments")
 public class CommentController {
-
+    @Autowired
+    CommentRepository commentRepository;
 
 }
