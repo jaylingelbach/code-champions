946e4491e83fa60ccbccf59cf9e5f28ab88274a9
Emily <emily.zy.lin@gmail.com>
2024-01-17 17:13:11 -0600

    extended CommentRespository from JPA

--- back-end/src/main/java/com/codechampions/easytravel/repository/CommentRepository.java
+++ back-end/src/main/java/com/codechampions/easytravel/repository/CommentRepository.java
@@ -1,4 +1,10 @@
 package com.codechampions.easytravel.repository;
 
-public interface CommentRepository {
+import com.codechampions.easytravel.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
 }
