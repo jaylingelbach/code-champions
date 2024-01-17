10834106787767c0ed88577f703a51ca2dfdee93
Emily <emily.zy.lin@gmail.com>
2024-01-17 17:10:04 -0600

    created CommentRespository (empty)

--- back-end/src/main/java/com/codechampions/easytravel/controller/CommentController.java
+++ back-end/src/main/java/com/codechampions/easytravel/controller/CommentController.java
@@ -1,9 +1,12 @@
 package com.codechampions.easytravel.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/comments")
 public class CommentController {
+
+
 }
---
+++ back-end/src/main/java/com/codechampions/easytravel/repository/CommentRespository.java
@@ -0,0 +1,4 @@
+package com.codechampions.easytravel.repository;
+
+public interface CommentRespository {
+}
