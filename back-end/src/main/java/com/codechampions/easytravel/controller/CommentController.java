624580842e23fd3cd53cb3e05e8a722a31e60a99
Emily <emily.zy.lin@gmail.com>
2024-01-17 17:45:49 -0600

    created PostMapping for collecting data from users (in CommentController.java)

--- back-end/src/main/java/com/codechampions/easytravel/controller/CommentController.java
+++ back-end/src/main/java/com/codechampions/easytravel/controller/CommentController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -36,6 +38,9 @@ public class CommentController {
 
         return "comments/commentshowpage";
     }
+
+    @PostMapping("")
+    public String addComment(@ModelAttribute Comment newComment, HttpSession session) {
 
 
 }
