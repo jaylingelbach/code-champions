a247356f68655cd39191bdfb43d16302eba662f0
Emily <emily.zy.lin@gmail.com>
2024-01-17 16:53:19 -0600

    added activity object to comment model

--- back-end/src/main/java/com/codechampions/easytravel/model/Comment.java
+++ back-end/src/main/java/com/codechampions/easytravel/model/Comment.java
@@ -1,7 +1,12 @@
 package com.codechampions.easytravel.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Comment extends AbstractEntity{
+    @ManyToOne
+    @JoinColumn(name = "activity_id")
+    private Activity activity;
 }
