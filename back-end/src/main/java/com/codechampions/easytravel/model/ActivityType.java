89c889460c0eaec82dc27c985f7038c6e84394fc
Emily <emily.zy.lin@gmail.com>
2024-01-18 12:15:25 -0600

    added the list of Activity to ActivityType Class

--- back-end/src/main/java/com/codechampions/easytravel/model/ActivityType.java
+++ back-end/src/main/java/com/codechampions/easytravel/model/ActivityType.java
@@ -1,10 +1,17 @@
 package com.codechampions.easytravel.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class ActivityType extends AbstractEntity{
 
     private String name;
+
+    @ManyToMany(mappedBy = "activityTypes")
+    private final List<Activity> activities = new ArrayList<>();
 
 }
