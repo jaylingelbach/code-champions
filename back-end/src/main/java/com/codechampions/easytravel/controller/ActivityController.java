ffc8f9241ceeeed2b13679b4a018fb8bb324c3de
Emily <emily.zy.lin@gmail.com>
2024-01-18 13:05:43 -0600

    added @GetMapping to display the activity list

--- back-end/src/main/java/com/codechampions/easytravel/controller/ActivityController.java
+++ back-end/src/main/java/com/codechampions/easytravel/controller/ActivityController.java
@@ -1,15 +1,16 @@
 package com.codechampions.easytravel.controller;
 
+import com.codechampions.easytravel.model.ActivityType;
+import com.codechampions.easytravel.model.Operator;
 import com.codechampions.easytravel.repository.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import com.codechampions.easytravel.model.Activity;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/activities")
@@ -31,6 +32,25 @@ public class ActivityController {
     private UserRepository userRepository;
 
     private static final String userSessionKey = "user";
+
+    @GetMapping("")
+    public String displayActivities(@RequestParam(required = false) Integer operatorId, @RequestParam(required = false) Integer activityTypeId, Model model) {
+        if (operatorId != null) {
+            Optional<Operator> operatorOptional = operatorRepository.findById(operatorId);
+            if (operatorOptional.isPresent()) {
+                Operator operator = operatorOptional.get();
+                model.addAttribute("activities", operator.getActivities());
+            }
+        } else if (activityTypeId != null) {
+            Optional<ActivityType> activityTypeOptional = activityTypeRepository.findById(activityTypeId);
+            if (activityTypeOptional.isPresent()) {
+                ActivityType activityType = activityTypeOptional.get();
+                model.addAttribute("activities", activityType.getActivities());
+            }
+        } else {
+            model.addAttribute("activities", activityRepository.findAll());
+        }
+        return "activities/index";
 
     @PostMapping("/activity")
     Activity newActivity(@RequestBody Activity newActivity){
